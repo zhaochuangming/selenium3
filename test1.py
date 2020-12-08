243037a57cbb0f1ecd47cde731426e72c394a2b2
v_cmzhao <v_cmzhao@digitalgd.com.cn>
2020-12-08 15:00:30 +0800

    test1.py

--- test1.py
+++ test1.py
@@ -2,7 +2,22 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+from time import sleep
+from selenium.webdriver.common.by import By
 cc = webdriver.Chrome()
-cc.get("http://www.baidu.com")
-print(WebDriverWait(cc,10).until(EC.title_is("百度一下，你就知道")))
+cc.get("https://mail.qq.com/")
+print(WebDriverWait(cc,10).until(EC.title_is("登录QQ邮箱")))
+
+#知识点：class_name定位出现多个值处理方法
+#1.先去console输入代码：document.getElementsByClassName("")，然后找到对应的下标
+cc.switch_to.frame(1)
+sleep(1)
+# cc.find_elements_by_class_name("inputstyle")[0].send_keys("1030253545@qq.com")
+# cc.find_elements_by_class_name("inputstyle")[1].send_keys("Cczcmily926520...")
+# cc.find_element_by_class_name("login_button").click()
+
+#知识点：判断元素是否可见
+kj = (By.CLASS_NAME,"inputstyle")
+print(WebDriverWait(cc,1).until(EC.visibility_of_element_located(kj)))
+cc.close()
+
